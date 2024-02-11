b86fec132434c45b07b6883d1aef446b6b492c7e
Kayoto0 <148847991+Kayoto0@users.noreply.github.com>
2024-02-11 09:03:17 -0500

    Update questions.js

--- js/questions.js
+++ js/questions.js
@@ -55,6 +55,17 @@ let questions = [
       "A LIL"
     ]
   },
+{
+    numb: 6,
+    question: "Are you fine",
+    answer: "Maybe",
+    options: [
+      "Yes",
+      "Yes",
+      "Yes",
+      "Maybe"
+    ]
+  },
 
   // you can uncomment the below codes and make duplicate as more as you want to add question
   // but remember you need to give the numb value serialize like 1,2,3,5,6,7,8,9.....
