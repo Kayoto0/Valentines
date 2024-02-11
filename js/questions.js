90fceb0f986cc0aebd3b95ae2a5e32bf4fe16704
Kayoto0 <148847991+Kayoto0@users.noreply.github.com>
2024-02-10 20:22:11 -0500

    Add files via upload

---
+++ js/questions.js
@@ -0,0 +1,51 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "When was our first Anniversary",
+    answer: "July 22",
+    options: [
+      "July 22",
+      "June 22",
+      "August 22",
+      "October 22"
+    ]
+  },
+  {
+    numb: 2,
+    question: "My favourite person",
+    answer: "Ti-ikha",
+    options: [
+      "Nobody",
+      "Ti-ikher",
+      "Ti-ikha",
+      "Myself"
+    ]
+  },
+  {
+    numb: 2,
+    question: "The Whoring Cat????",
+    answer: "Star",
+    options: [
+      "Sun",
+      "Moon",
+      "Star",
+      "Neptune"
+    ]
+  },
+
+  // you can uncomment the below codes and make duplicate as more as you want to add question
+  // but remember you need to give the numb value serialize like 1,2,3,5,6,7,8,9.....
+
+  //   {
+  //   numb: 6,
+  //   question: "Your Question is Here",
+  //   answer: "Correct answer of the question is here",
+  //   options: [
+  //     "Option 1",
+  //     "option 2",
+  //     "option 3",
+  //     "option 4"
+  //   ]
+  // },
+];
