422d24d79b1f2ae449a59d0d5b2f5d75f38fbf36
Kayoto0 <148847991+Kayoto0@users.noreply.github.com>
2024-02-12 19:45:58 -0500

    Update questions.js

--- js/questions.js
+++ js/questions.js
@@ -1,6 +1,6 @@
 // creating an array and passing the number, questions, options, and answers
 let questions = [
-    {
+{
     numb: 1,
     question: "When was our first Anniversary",
     answer: "July 22",
