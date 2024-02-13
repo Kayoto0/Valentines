e87996f3b6a198e441a1b86bd07f4200ea77aa08
Kayoto0 <148847991+Kayoto0@users.noreply.github.com>
2024-02-13 06:01:24 -0500

    Update questions.js

--- js/questions.js
+++ js/questions.js
@@ -1,6 +1,6 @@
 // creating an array and passing the number, questions, options, and answers
 let questions = [
-{
+    {
     numb: 1,
     question: "When was our first Anniversary",
     answer: "July 22",
@@ -22,94 +22,52 @@ let questions = [
       "Myself"
     ]
   },
-{
+  {
     numb: 3,
-    question: "The part of you I compliment the MOST",
-    answer: "Your eyes",
+    question: "The Whoring Cat????",
+    answer: "Star",
     options: [
-      "Your Eyes",
-      "Your Ta Tas",
-      "Your bum bum",
-      "Your Sniffy sniff"
+      "Sun",
+      "Moon",
+      "Star",
+      "Neptune"
     ]
   },
-{
+   {
     numb: 4,
-    question: "How much do I love you",
-    answer: "INFINITLY ",
+    question: "The part f you i love the most",
+    answer: "Your Voice",
     options: [
-      "ALLLLLLOT ",
-      "NOT ENOUGH",
-      "INFINITLY ",
-      "A LIL"
+      "Your Ta Tas",
+      "Your Bum Bum",
+      "Your lips",
+      "Your Voice"
     ]
   },
-{
+ {
     numb: 5,
-    question: "Are you fine",
-    answer: "Maybe",
+    question: "Wedding Certificate",
+    answer: "Jan 13",
     options: [
-      "Yes",
-      "Yes",
-      "Yes",
-      "Maybe"
+      "Jan 15",
+      "Jan 14",
+      "Jan 13",
+      "Jan 17"
     ]
   },
-{
+   {
     numb: 6,
-    question: "2 plus 2",
-    answer: "49",
+    question: "TWioll you be my Valentines Next Yeat",
+    answer: "Yes",
     options: [
-      "48",
-      "4",
-      "99 ",
-      "49"
-    ]
-  },
-{
-    numb: 7,
-    question: "The first physical gift I made for you",
-    answer: "A ring",
-    options: [
-      "Idk ",
-      "A chain",
-      "A ring ",
-      "Toes"
-    ]
-  },
-{
-    numb: 8,
-    question: "How long we've been together ",
-    answer: "7 months and 20 days",
-    options: [
-      "....",
-      "7 months 15 days",
-      "8 months ",
-      "7 months 20 days"
-    ]
-  },
-{
-    numb: 9,
-    question: "What do we call potato ",
-    answer: "❤️📸 ",
-    options: [
-      "😭😍",
-      "😏",
-      "❤️📸 ",
-      "😚💞"
-    ]
-  },
-{
-    numb: 10,
-    question: "Let's make out in my bed",
-    answer: "Sure",
-    options: [
-      "Sure",
-      "YESSS PLEASE",
       "Yes",
-      "Ew no☹️"
+      "Yes",
+      "Yes",
+      "Yes"
     ]
-  },  // you can uncomment the below codes and make duplicate as more as you want to add question
+  },
+
+  // you can uncomment the below codes and make duplicate as more as you want to add question
   // but remember you need to give the numb value serialize like 1,2,3,5,6,7,8,9.....
 
   //   {
